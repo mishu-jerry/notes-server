8a52e95b557fe7921f4f2dced6c638b6467c6435
mishu-jerry <barman.mishujerry@gmail.com>
2020-10-27 18:02:00 +0600

    add comments

--- middleware/async.js
+++ middleware/async.js
@@ -1,3 +1,21 @@
+/**
+ * Wraps the 'handler' middlware function with try-catch block.
+ * 
+ * Helps to avoid writing try-catch blocks in the middlewares
+ * functions. Useful for writting a cleaner code.
+ * 
+ * ----------------------------------------------
+ * const asyncMiddlware = require('async');
+ * 
+ * asyncMiddleware(async (req, res, next) => {
+ * 
+ *   // Do something (throws exception)
+ *   // Don't need to use try-catch, next(err), they will be
+ *   // handled by asyncMiddleware()
+ * 
+ * });
+ * ----------------------------------------------
+ */
 
 module.exports = function (handler) {
     return async (req, res, next) => {
